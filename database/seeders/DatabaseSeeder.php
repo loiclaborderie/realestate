e53cf839ef2adf007eff1a1ed1a418a07f88e852
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-07-06 14:40:08 +0700

    chore: filled db seeder with new factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\PropertyType;
 use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use phpDocumentor\Reflection\DocBlock\Tags\Property;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $roles = ["admin", "seller", "buyer"];
+        $property_types = ["House", "Appartment", "Building", "Office"];
         foreach ($roles as $role) {
             Role::create(['name' => $role]);
         }
+        foreach ($property_types as $type) {
+            PropertyType::create(['name' => $type]);
+        }
 
-        User::factory()->admin()->create([
+        $me = User::factory()->admin()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
@@ -27,5 +33,14 @@ class DatabaseSeeder extends Seeder
         User::factory()->seller()->createMany(10);
 
         User::factory()->createMany(20);
+
+        $user_ids = User::all()->pluck('id')->toArray();
+        $property_type_ids = PropertyType::all()->pluck('id')->toArray();
+
+        \App\Models\Property::factory()->count(10)->make()->each(function ($property) use ($user_ids, $property_type_ids) {
+            $property->user_id = $user_ids[array_rand($user_ids)];
+            $property->property_type_id = $property_type_ids[array_rand($property_type_ids)];
+            $property->save();
+        });
     }
 }
