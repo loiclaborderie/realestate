e27501c5e6c1f736a52a8662afc5662114c23c05
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-07-13 16:49:45 +0700

    chore: added controller to update property only (not the type / address)

--- app/Http/Controllers/Property/UpdateController.php
+++ app/Http/Controllers/Property/UpdateController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Property;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PropertyUpdateRequest;
+use App\Models\Property;
+use Inertia\Inertia;
 
 class UpdateController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Property $property, PropertyUpdateRequest $request)
     {
-        return "update access";
+        $property->update($request->validated());
+        return Inertia::render('Property/MyPropertiesList');
     }
 }
