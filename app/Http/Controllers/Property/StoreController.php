78a8546b2de4f4caece21e1fd4469bc84365630f
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-07-13 16:49:18 +0700

    chore: added controller to create property with it's address

--- app/Http/Controllers/Property/StoreController.php
+++ app/Http/Controllers/Property/StoreController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers\Property;
 
+use App\Actions\CreatePropertyAddressAction;
+use App\DTOs\PropertyAddressData;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PropertyStoreRequest;
+use App\Models\PropertyType;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(PropertyStoreRequest $request, CreatePropertyAddressAction $action)
     {
-        return "store access";
+        $addressData = PropertyAddressData::fromArray($request->validated());
+        try {
+            $propertyAddress = $action->execute($addressData);
+        } catch (\Exception $e){
+            return redirect()->back()->withErrors(['message' => $e->getMessage()]);
+        }
+        $propertyData = $request->only([
+            'name',
+            'price',
+            'bedroom',
+            'bathroom',
+            'floor',
+            'building_area',
+            'land_area',
+            'sold_at'
+        ]);
+
+        // TODO: Add picture upload as well
+
+        Auth::user()->properties()->create([
+            'property_address_id' => $propertyAddress->id,
+            'property_type_id' => PropertyType::firstWhere('name', $request['type'])->id,
+            ...$propertyData
+        ]);
+        return Inertia::render('Property/MyPropertiesList');
+
     }
 }
