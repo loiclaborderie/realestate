d2cfde46caff190c5be0ae02b5abac25ec2049ce
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-07-13 15:48:38 +0700

    chore: created action to create / update property address using the geocoding service

---
+++ app/Actions/CreatePropertyAddressAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Actions;
+
+use App\DTOs\PropertyAddressData;
+use App\Models\PropertyAddress;
+use App\Services\GeocodeMapService;
+
+class CreatePropertyAddressAction
+{
+    public function __construct(private readonly GeocodeMapService $geocodingService)
+    {
+    }
+
+    public function execute(PropertyAddressData $data): PropertyAddress
+    {
+        $fullAddress = "{$data->street} {$data->city} {$data->state} {$data->zip} {$data->country}";
+
+        $coordinates = $this->geocodingService->getCoordinates($fullAddress);
+
+        if (empty($coordinates)) {
+            throw new \Exception('Unable to determine the latitude and longitude for the provided address.');
+        }
+
+        return PropertyAddress::create([
+            'country' => $data->country,
+            'state' => $data->state,
+            'city' => $data->city,
+            'street' => $data->street,
+            'zip' => $data->zip,
+            'unit_number' => $data->unitNumber,
+            'latitude' => $coordinates['latitude'],
+            'longitude' => $coordinates['longitude'],
+        ]);
+    }
+}
