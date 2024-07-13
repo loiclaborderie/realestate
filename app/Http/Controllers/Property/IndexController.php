7bf87b69444667c544ced916ba816d604fb790fd
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-07-13 16:52:48 +0700

    chore: added basic index controller for properties

--- app/Http/Controllers/Property/IndexController.php
+++ app/Http/Controllers/Property/IndexController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers\Property;
 
 use App\Http\Controllers\Controller;
+use App\Models\Property;
+use Illuminate\Http\Request;
+use Illuminate\Support\Benchmark;
+use Inertia\Inertia;
 
 class IndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        return "index access";
+        // TODO: Add more query params for filtrers, search etc
+        $lat = $request->input('lat', null);
+        $long = $request->input('long', null);
+        $query = Property::with('address');
+        if(!is_null($lat) && !is_null($long)){
+            $query->withDistance($long, $lat);
+        }
+        $data = $query->paginate(10);
+
+        return Inertia::render('Property/Index', [
+            'properties'=> $data->items()
+        ]);
     }
 }
