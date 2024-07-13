355ec1258737f06c93f4b83bea652877ac6af901
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-07-13 16:50:58 +0700

    chore: added scope with distance to find closest properties and their distance in km to a given coordinate

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Property extends Model
 {
@@ -43,5 +43,17 @@ class Property extends Model
     public function pictures() : HasMany
     {
         return $this->hasMany(PropertyPicture::class);
+    }
+
+    public function scopeWithDistance(Builder $query, float $long, float $lat): Builder
+    {
+        return $query->select('properties.*')
+            ->selectRaw('( 6371 * acos( cos( radians(?) )
+             * cos( radians( property_addresses.latitude ) )
+             * cos( radians( property_addresses.longitude ) - radians(?) )
+             + sin( radians(?) ) * sin( radians( property_addresses.latitude )
+              ) ) ) AS distance', [$lat, $long, $lat])
+            ->join('property_addresses', 'properties.property_address_id', '=', 'property_addresses.id')
+            ->orderBy('distance');
     }
 }
